c301ec02c0a31b69be576d1d07a0a1c97b050155
Victor Akintunde <akintoluvic@gmail.com>
2025-07-29 16:14:13 +0100

    Comment out PrimaryFeatures component in Home for future reference

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
-        <PrimaryFeatures />
+        {/* <PrimaryFeatures /> */}
         <SecondaryFeatures />
         <CallToAction />
         <Testimonials />
