9d6bbf6de0c3fa2b5b8f554c5729f98cf30f4c49
Victor Akintunde <akintoluvic@gmail.com>
2025-07-30 17:14:58 +0100

    Update heading in PrimaryFeatures component for clarity

--- src/components/PrimaryFeatures.tsx
+++ src/components/PrimaryFeatures.tsx
@@ -82,7 +82,7 @@ export function PrimaryFeatures() {
       <Container className="relative">
         <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
-            Business solutions for every need.
+            Our business solutions.
           </h2>
           <p className="mt-6 text-lg tracking-tight text-blue-100">
             Our services are designed to streamline operations and enhance
